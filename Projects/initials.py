6cbc0555b2da95f3d4c7bdbce170b5ae0550e567
Epic91 <hernandeze791@gmail.com>
2021-12-22 20:15:54 -0600

    first project

---
+++ Projects/initials.py
@@ -0,0 +1,24 @@
+# ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
+# Write a Python program called initials.py that displays the initials of your name in block letters as shown and dip your toes into ASCII art.
+
+first_initial = '''
+EEEEE
+E
+E
+EEE
+E
+E
+EEEEE
+'''
+
+last_initial = '''
+H   H
+H   H
+H   H
+HHHHH
+H   H
+H   H
+H   H
+'''
+
+print(first_initial + last_initial)
