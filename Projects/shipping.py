6a33622a3b2c6a03635d75ae0a6d3a24ad195650
Epic91 <hernandeze791@gmail.com>
2021-12-30 00:16:29 -0600

    new project conditionals and boolean operators

---
+++ Projects/shipping.py
@@ -0,0 +1,37 @@
+'''
+In this project, you’ll build a program that will take the weight of a package and determine the cheapest way to ship that package using Sal’s Shippers.
+
+Sal’s Shippers has several different options for a customer to ship their package:
+
+Ground Shipping, which is a small flat charge plus a rate based on the weight of your package.
+Ground Shipping Premium, which is a much higher flat charge, but you aren’t charged for weight.
+Drone Shipping (new), which has no flat charge, but the rate based on weight is triple the rate of ground shipping.
+'''
+
+weight = 4.8
+premium_shipping = 125.00
+
+# Ground Shipping
+if weight <= 2:
+  cost_ground = weight * 1.5 + 20
+elif weight <= 6:
+  cost_ground = weight * 3.00 + 20
+elif weight <= 10:
+  cost_ground = weight * 4.00 + 20
+else:
+  cost_ground = weight * 4.75 + 20
+
+
+# Drone Shipping
+if weight <= 2:
+  cost_drone = weight * 4.50 
+elif weight <= 6:
+  cost_drone = weight * 9.00
+elif weight <= 10:
+  cost_drone = weight * 12.00
+else:
+  cost_drone = weight * 14.25
+
+print("Ground Shipping: $", cost_ground)
+print("Ground Shipping Premium: $", premium_shipping)
+print("Drone Shipping: $", cost_drone)
