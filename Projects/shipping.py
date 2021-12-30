9448437ef915c61bfed2b9279fb3b2bcb26ec908
Epic91 <hernandeze791@gmail.com>
2021-12-30 00:32:07 -0600

    removed multi line and added # on each line for comment

--- Projects/shipping.py
+++ Projects/shipping.py
@@ -1,12 +1,10 @@
-'''
-In this project, you’ll build a program that will take the weight of a package and determine the cheapest way to ship that package using Sal’s Shippers.
+# In this project, you’ll build a program that will take the weight of a package and determine the cheapest way to ship that package using Sal’s Shippers.
 
-Sal’s Shippers has several different options for a customer to ship their package:
+# Sal’s Shippers has several different options for a customer to ship their package:
 
-Ground Shipping, which is a small flat charge plus a rate based on the weight of your package.
-Ground Shipping Premium, which is a much higher flat charge, but you aren’t charged for weight.
-Drone Shipping (new), which has no flat charge, but the rate based on weight is triple the rate of ground shipping.
-'''
+# Ground Shipping, which is a small flat charge plus a rate based on the weight of your package.
+# Ground Shipping Premium, which is a much higher flat charge, but you aren’t charged for weight.
+# Drone Shipping (new), which has no flat charge, but the rate based on weight is triple the rate of ground shipping.
 
 weight = 4.8
 premium_shipping = 125.00
